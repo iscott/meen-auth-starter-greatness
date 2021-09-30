6786c4b6c271b9a285ba0f801304c2d93c6786bb
Ira Herman <ira@xynetik.com>
2021-09-30 12:29:36 -0700

    Adds user create action

--- controllers/users.js
+++ controllers/users.js
@@ -7,6 +7,16 @@ const User = require('../models/user.js');
 // New (registration page)
 
 // Create (registration route)
+userRouter.post('/', (req, res) => {
+  //overwrite the user password with the hashed password, then pass that in to our database
+  req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(12));
+
+  User.create(req.body, (error, createdUser) => {
+    res.send(createdUser);
+  });
+
+});
+
 
 // Export User Router
 module.exports = userRouter;
