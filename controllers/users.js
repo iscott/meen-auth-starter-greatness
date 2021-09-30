03f25a077a793e959636642479046a1774281916
Ira Herman <ira@xynetik.com>
2021-09-30 11:51:49 -0700

    Adds user model and users controller

---
+++ controllers/users.js
@@ -0,0 +1,12 @@
+// Dependencies
+const bcrypt = require('bcrypt');
+const express = require('express');
+const userRouter = express.Router();
+const User = require('../models/user.js');
+
+// New (registration page)
+
+// Create (registration route)
+
+// Export User Router
+module.exports = userRouter;
--- server.js
+++ server.js
@@ -19,6 +19,14 @@ db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
 db.on('connected', () => console.log('mongo connected'));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
+// Middleware
+// Body parser middleware: give us access to req.body
+app.use(express.urlencoded({ extended: true }));
+
+// Routes / Controllers
+const userController = require('./controllers/users');
+app.use('/users', userController);
+
 // Listener
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`server is listening on port: ${PORT}`));
