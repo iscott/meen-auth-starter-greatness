249ecbe8d60cc7ecfc679c68a8a3ae6866bdbee6
Ira Herman <ira@xynetik.com>
2021-09-30 11:43:14 -0700

    Initial express boilerplate

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+// Dependencies
+const express = require('express');
+const app = express();
+
+// Dependencies 
+const mongoose = require('mongoose');
+
+// Database Configuration
+mongoose.connect(process.env.DATABASE_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+// Database Connection Error / Success
+const db = mongoose.connection;
+db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+// Listener
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`server is listening on port: ${PORT}`));
